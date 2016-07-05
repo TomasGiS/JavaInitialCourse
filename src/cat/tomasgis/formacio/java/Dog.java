b035e560a9fc31b41b4b7ee335afec74eda5e11a
TomasGiS <me@tomasgis.cat>
2016-07-05 07:04:08 +0200

    Dog class declaration

---
+++ src/cat/tomasgis/formacio/java/Dog.java
@@ -0,0 +1,21 @@
+package cat.tomasgis.formacio.java;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * Created by TomasGiS on 4/7/16.
+ */
+public class Dog {
+
+
+    String getName;
+    String ownerName;
+    int plateNumber;
+    String skinColor;
+    float weight;
+    long birthDay;
+
+
+}
--- src/cat/tomasgis/formacio/java/Main.java
+++ src/cat/tomasgis/formacio/java/Main.java
@@ -1,8 +1,15 @@
 package cat.tomasgis.formacio.java;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Dog doggy = new Dog();
+
+        //Els atributs/fields són privats per defecte
+        //no es poden modificar fora de la classe Dog
     }
 }
