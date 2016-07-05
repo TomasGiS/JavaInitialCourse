db35e205f02dd77d2ebf210b923c1cb3a4637935
Tomas GiS <TomasGiS@Vanagandr.local>
2016-07-05 14:42:38 +0200

    Cat and Dog interface behaviour

---
+++ src/cat/tomasgis/formacio/java/interfaces/Miaow.java
@@ -0,0 +1,8 @@
+package cat.tomasgis.formacio.java.interfaces;
+
+/**
+ * Created by TomasGiS on 5/7/16.
+ */
+public interface Miaow {
+    public void miaowing(int times);
+}
---
+++ src/cat/tomasgis/formacio/java/interfaces/iHowl.java
@@ -0,0 +1,8 @@
+package cat.tomasgis.formacio.java.interfaces;
+
+/**
+ * Created by TomasGiS on 5/7/16.
+ */
+public interface iHowl {
+    public void howling(int times);
+}
